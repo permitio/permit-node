22c3071b79e98db92ce999f0feed45afda69a84c
orweis <orweis@gmail.com>
2020-12-29 23:48:58 +0200

    auto register resources

--- src/appManager.ts
+++ src/appManager.ts
@@ -1,11 +1,16 @@
+import _ from 'lodash';
+
 import { mapApp } from './appMapper/appMapper';
+import { resource as saveResource } from './commands';
 
 export class AppManager {
   resources: Record<string, any> = {};
 
   manage(name: string, app: any): void {
     // TODO support mixed HTTP/HTTPS server (via name merging resources mutual to http and https )
-    this.resources[name] = mapApp(app);
+    const resources = mapApp(app);
+    this.resources[name] = resources;
+    _.forEach(resources, (resource) => saveResource(resource));
   }
 }
 
