348575fdfe7118fe85a5ce9209fe2ab66c578f32
orweis <orweis@gmail.com>
2020-12-29 23:56:31 +0200

    comments

--- src/appManager.ts
+++ src/appManager.ts
@@ -2,15 +2,19 @@ import _ from 'lodash';
 
 import { mapApp } from './appMapper/appMapper';
 import { resource as saveResource } from './commands';
+import { config } from './config';
 
 export class AppManager {
   resources: Record<string, any> = {};
 
   manage(name: string, app: any): void {
     // TODO support mixed HTTP/HTTPS server (via name merging resources mutual to http and https )
-    const resources = mapApp(app);
-    this.resources[name] = resources;
-    _.forEach(resources, (resource) => saveResource(resource));
+    // If we have auto-mapping - map the app given to us and save its resources
+    if (config.autoMapping) {
+      const resources = mapApp(app);
+      this.resources[name] = resources;
+      _.forEach(resources, (resource) => saveResource(resource));
+    }
   }
 }
 
