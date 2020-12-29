4e5a7e435453e1bca8a4507a5c9c44d28ac394a9
orweis <orweis@gmail.com>
2020-12-29 21:55:08 +0200

    Upgrade hooker

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,41 +1,43 @@
 import Hook from 'require-in-the-middle';
 
 import { mapApp } from './appMapper/appMapper';
+import { logger } from './logger';
 
 /**
  * Plugin (hook) into frameworks (Express) and track created apps
  */
 export function hook() {
-  // Hook(['express'], undefined, (exports: express.Express) => {
-  //   const handler = {
-  //     apply: (target: any, thisArg: any, argumentsList: any[]) => {
-  //       const app = target.apply(thisArg, argumentsList);
-  //       trackedApps.push(app);
-  //       return app;
-  //     },
-  //   };
-
-  //   const proxy = new Proxy(exports, handler);
-  //   return proxy;
-  // });
-
   //  Plugin into the http/https create server and map the app provided there/then
-  Hook(['http', 'https'], undefined, (exports: any) => {
+  Hook(['http', 'https'], undefined, (exports: any, name: string) => {
+    /**
+     * Handler for the proxy
+     */
     const handler = {
       get: function (target: any, prop: any, receiver: any) {
-        // Wrap call for createServer - to catch passed app
-        if (prop === 'createServer') {
-          return (listener: any) => {
-            mapApp(listener);
-            // Call original
-            return Reflect.get(target, prop, receiver)(listener);
-          };
+        try {
+          // Wrap call for createServer - to catch passed app
+          if (prop === 'createServer') {
+            return (listener: any) => {
+              mapApp(listener);
+              // Proxy the call to the original app
+              return Reflect.get(target, prop, receiver)(listener);
+            };
+          }
+        } catch (error) {
+          logger.error('Failed to plugin into http/https', { error });
         }
+        // if not 'creatServer' or if we failed we return the original property
         return Reflect.get(target, prop, receiver);
       },
     };
 
-    const proxy = new Proxy(exports, handler);
-    return proxy;
+    /** Create a Proxy and wrap the module */
+    try {
+      const proxy = new Proxy(exports, handler);
+      return proxy;
+    } catch (error) {
+      logger.error('Plugin hook failed to proxy module', { name, error });
+      return exports;
+    }
   });
 }
