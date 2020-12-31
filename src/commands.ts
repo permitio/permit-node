0851ea3cbd8c9b2b7a1656fca844e27db4c602c7
Asaf Cohen <asafc@me.com>
2020-12-31 22:33:28 +0200

    fix stub.action() api

--- src/commands.ts
+++ src/commands.ts
@@ -2,23 +2,8 @@ import { authorizationClient, AuthorizonConfig, ResourceStub } from './client';
 import { config } from './config';
 import { logger } from './logger';
 import { ActionDefinition, ResourceDefinition } from './registry';
-
-export interface ResourceConfig {
-  name: string;
-  type: string;
-  path: string;
-  description?: string;
-  actions?: ActionDefinition[];
-  attributes?: Record<string, any>;
-}
-
-export interface ActionConfig {
-  name: string;
-  title?: string;
-  description?: string;
-  path?: string;
-  attributes?: Record<string, any>;
-}
+import { ActionConfig, ResourceConfig } from './interface';
+export { ActionConfig, ResourceConfig } from './interface';
 
 export const init = (configOptions: AuthorizonConfig): void => {
   logger.info(`authorizon.init(), sidecarUrl: ${config.sidecarUrl}`);
