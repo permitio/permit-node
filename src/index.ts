ec636c3205cccd7de0398eb4b9302cb7edd304c7
Asaf Cohen <asafc@me.com>
2021-02-21 19:17:59 +0200

    expose new api

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,15 @@ hook();
 const client = authorizationClient;
 export const syncUser = client.syncUser.bind(client);
 export const deleteUser = client.deleteUser.bind(client);
+export const getUser = client.getUser.bind(client);
 export const syncOrg = client.syncOrg.bind(client);
 export const deleteOrg = client.deleteOrg.bind(client);
+export const getOrg = client.getOrg.bind(client);
 export const addUserToOrg = client.addUserToOrg.bind(client);
 export const removeUserFromOrg = client.removeUserFromOrg.bind(client);
 export const getOrgsForUser = client.getOrgsForUser.bind(client);
 export const getUserRoles = client.getUserRoles.bind(client);
+export const getRole = client.getRole.bind(client);
 export const assignRole = client.assignRole.bind(client);
 export const unassignRole = client.unassignRole.bind(client);
 export const updatePolicyData = client.updatePolicyData.bind(client);
@@ -34,6 +37,7 @@ export const getResourceAndAction = resourceRegistry.getResourceAndActionFromReq
 export const isUserSynced = client.isUserSynced.bind(client);
 export const getLocallyCachedUser = client.getLocallyCachedUser.bind(client);
 export const getLocallyCachedUserList = client.getLocallyCachedUserList.bind(client);
+export const getLocallyCachedUserOrgs = client.getLocallyCachedUserOrgs.bind(client);
 export const getLocallyCachedUserRoles = client.getLocallyCachedUserRoles.bind(client);
 export const getLocallyCachedRoleList = client.getLocallyCachedRoleList.bind(client);
 export const getLocallyCachedRoleById = client.getLocallyCachedRoleById.bind(client);
@@ -45,12 +49,15 @@ const authorizon = {
   action: action,
   syncUser: syncUser,
   deleteUser: deleteUser,
+  getUser: getUser,
   syncOrg: syncOrg,
   deleteOrg: deleteOrg,
+  getOrg: getOrg,
   addUserToOrg: addUserToOrg,
   removeUserFromOrg: removeUserFromOrg,
   getOrgsForUser: getOrgsForUser,
   getUserRoles: getUserRoles,
+  getRole: getRole,
   assignRole: assignRole,
   unassignRole: unassignRole,
   updatePolicyData: updatePolicyData,
@@ -64,6 +71,7 @@ const authorizon = {
   isUserSynced: isUserSynced,
   getLocallyCachedUser: getLocallyCachedUser,
   getLocallyCachedUserList: getLocallyCachedUserList,
+  getLocallyCachedUserOrgs: getLocallyCachedUserOrgs,
   getLocallyCachedUserRoles: getLocallyCachedUserRoles,
   getLocallyCachedRoleList: getLocallyCachedRoleList,
   getLocallyCachedRoleById: getLocallyCachedRoleById,
