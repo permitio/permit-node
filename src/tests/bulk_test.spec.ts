1cc066f128195eb156aa2441e93865573d8afea9
mayabarak <maya@permit.io>
2024-02-22 10:45:22 +0200

    add test

--- src/tests/bulk_test.spec.ts
+++ src/tests/bulk_test.spec.ts
@@ -11,28 +11,26 @@ test('Bulk relationship tuples test', async (t) => {
   try {
     const tuples = [
       {
-        subject: 'user:alice',
-        relation: 'can_access',
-        object: 'resource:document',
-        tenant: 'public',
+        subject: 'folders:pdf',
+        relation: 'parent',
+        object: 'docs:tasks',
+        tenant: 'default',
       },
       {
-        subject: 'user:bob',
-        relation: 'can_access',
-        object: 'resource:document',
-        tenant: 'public',
+        subject: 'folders:png',
+        relation: 'parent',
+        object: 'docs:files',
+        tenant: 'default',
       },
     ];
-
     logger.info('Tuples: ' + JSON.stringify(tuples));
 
     const result = await permit.api.relationshipTuples.bulkRelationshipTuples(tuples);
     logger.info('Result:', result);
 
     const resultLength = result.operations.length;
-    t.is(resultLength, 2);
+    t.is(resultLength, 1);
     t.deepEqual(result.operations[0], tuples[0]);
-    t.deepEqual(result.operations[1], tuples[1]);
     logger.info('Bulk relationship tuples test passed');
   } catch (error) {
     logger.error(`Got error: ${error}`);
