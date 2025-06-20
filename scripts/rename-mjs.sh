63d9a600ce16473ae03c9e412fa6a160352d9027
Wesley Matos <wesleymatosdev@gmail.com>
2025-06-20 14:53:06 -0300

    build: improve rename-mjs script

--- scripts/rename-mjs.sh
+++ scripts/rename-mjs.sh
@@ -1,6 +1,9 @@
-find ./build/module -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.mjs"' {} \;
-
-find ./build/module -name '*.mjs' -exec sed -i -e "s/\.js'/.mjs'/g" {} \;
+# This script renames all import files extensions from `.js` to `.mjs`
+# It's important to run this first, as if we run this after the conversion to `.mjs`
+# we may do unnecessary work by reworking files that have already been converted.
+find ./build/module -name '*.js' -exec sed -i -e "s/\.js'/.mjs'/g" {} \;
 
 # files with `.mjs-e` extension are created by the command above.
-find ./build/module -name '*.mjs-e' -exec rm {} \;
+find ./build/module -name '*.js-e' -exec rm {} \;
+
+find ./build/module -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.mjs"' {} \;
