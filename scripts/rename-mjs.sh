cb494711ce1366b1843948c35161becb87b2723b
Wesley Matos <wesleymatosdev@gmail.com>
2025-06-20 11:48:36 -0300

    feat: rename import extension to `.mjs` for ESModules

--- scripts/rename-mjs.sh
+++ scripts/rename-mjs.sh
@@ -1,3 +1,6 @@
 find ./build/module -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.mjs"' {} \;
 
+find ./build/module -name '*.mjs' -exec sed -i -e "s/\.js'/.mjs'/g" {} \;
 
+# files with `.mjs-e` extension are created by the command above.
+find ./build/module -name '*.mjs-e' -exec rm {} \;
